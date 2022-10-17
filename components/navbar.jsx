f1e73bee8a1482267d24ffae7483911b1abf63c4
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2022-10-16 22:07:44 -0300

    add-iconlinks

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Image from 'next/image';
 import LogoFlock from '../src/assets/svg/logo-flock-tech.png';
+import Link from 'next/link'
 
 export default function index() {
 
@@ -11,16 +12,21 @@ export default function index() {
                 {/* For large and Medium-sized Screen */}
                 <div className="flex justify-between ">
                     <div className="hidden sm:flex flex-row items-center space-x-6">
-                       
+
+                        <Link href='https://web.facebook.com/flockmarketingeventos/?_rdc=10&_rdr'>
                         <svg className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer" width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M18 2H15C13.6739 2 12.4021 2.52678 11.4645 3.46447C10.5268 4.40215 10 5.67392 10 7V10H7V14H10V22H14V14H17L18 10H14V7C14 6.73478 14.1054 6.48043 14.2929 6.29289C14.4804 6.10536 14.7348 6 15 6H18V2Z" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
-                       
+                        </Link>
+                        
+                        <Link href='https://www.instagram.com/flockmkt/'>
                         <svg className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 cursor-pointer" width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M17 2H7C4.23858 2 2 4.23858 2 7V17C2 19.7614 4.23858 22 7 22H17C19.7614 22 22 19.7614 22 17V7C22 4.23858 19.7614 2 17 2Z" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M16.0002 11.3701C16.1236 12.2023 15.9815 13.0523 15.594 13.7991C15.2065 14.5459 14.5933 15.1515 13.8418 15.5297C13.0903 15.908 12.2386 16.0397 11.408 15.906C10.5773 15.7723 9.80996 15.3801 9.21503 14.7852C8.62011 14.1903 8.22793 13.4229 8.09426 12.5923C7.9606 11.7616 8.09226 10.91 8.47052 10.1584C8.84878 9.40691 9.45438 8.7938 10.2012 8.4063C10.948 8.0188 11.7979 7.87665 12.6302 8.00006C13.4791 8.12594 14.265 8.52152 14.8719 9.12836C15.4787 9.73521 15.8743 10.5211 16.0002 11.3701Z" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
                             <path d="M17.5 6.5H17.51" stroke="#1F2937" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
+                        </Link>
+                      
                     </div>
                     <div className=" flex space-x-3 items-center">
                        <Image src={LogoFlock} width={50} height={50}></Image>
