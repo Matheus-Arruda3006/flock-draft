753c3f5578124e2c152162e64a3a6459da277ae3
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2022-10-16 22:16:35 -0300

    link-whats

--- components/footer.jsx
+++ components/footer.jsx
@@ -23,14 +23,15 @@ const Footer1 = () => {
                             Av. Miguel Sutil, 6274 - Consil, Cuiabá - MT, 78048-000
                         </span>
                     </p>
-
+                    <Link href='https://api.whatsapp.com/send?phone=5565993066069&text=Ol%C3%A1%2C%20vim%20pelo%20site%2C%20tenho%20interesse%20em%20saber%20mais...'>
                     <p class="flex items-start -mx-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                         </svg>
-
                         <span class="mx-2 text-gray-700 truncate w-72 dark:text-black-400">(65) 99907-5959</span>
                     </p>
+                    </Link>
+                   
 
                     <p class="flex items-start -mx-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mx-2 text-blue-500 dark:text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
