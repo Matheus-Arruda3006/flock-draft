118acfcd9af10ea0566db0b10548356d249e33be
Matheus de Arruda <matheusarrudaf@gmail.com>
2022-11-04 18:07:19 -0300

    commit-illustration

--- components/products.jsx
+++ components/products.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import {AiOutLineHeart} from 'react-icons/ai'
+import { AiOutLineHeart } from 'react-icons/ai'
+import Image from 'next/image';
+
 
 
 export default function IndexPage() {
@@ -17,10 +19,7 @@ export default function IndexPage() {
                             <div className="bg-white">
                                 <div className="flex items-center justify-between px-4 pt-4">
                                     <div>
-                                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                         d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5
-                                         4.5 0 00-6.364 0z"/></svg>
+                                       <Image src=""/>
                                     </div>
                                     <div className="bg-yellow-200 py-1.5 px-6 rounded-full">
                                         <p className="text-xs text-yellow-500">Novo</p>
